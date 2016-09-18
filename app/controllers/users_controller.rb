95850fbd80927c9d086bbec9774eb9bf1424bc7b
edwinfachri <fachrifachrifachri@gmail.com>
2016-09-18 10:56:39 +0700

    error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params) #not the final implementation
+    if @user.save
+      #Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
